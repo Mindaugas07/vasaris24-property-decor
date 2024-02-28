80953f7d65619e858c654f7acbd088f9b0c24faf
Mindaugas Minelga <minelga.m@gmail.com>
2024-02-28 21:01:10 +0200

    modify main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@
 # print(converter.celsius_to_fahrenheit, converter.celsius_to_kelvin)
 
 
-# Create a class that securely stores and manages passwords. 
+# Create a class that securely stores and manages passwords.
 # Use properties with getters and setters to ensure password security and prevent unauthorized access.
 
 # Requirements:
@@ -114,3 +114,179 @@
 #  - Create a getter for password that always returns randomly places 8 to 12 elements from hash function  (symbols must not be duplicated) to prevent direct access to the actual password.
 #  - Create a setter, that would take current password and new one, if current password is correct , would set new one, otherwise raise error.
 
+# from hashlib import sha256
+
+
+# class Passwords:
+#     PASSWORD_LENGTH = 7
+
+#     def __init__(self, password: str) -> None:
+#         if len(password) > self.PASSWORD_LENGTH:
+#             self._password = sha256(password.encode("utf-8")).hexdigest()
+#         else:
+#             raise ValueError("Password should be at least 8 characters long!")
+
+#     @property
+#     def password(self) -> str:
+#         return self._password
+
+#     @password.setter
+#     def set_password(self, new_password: str) -> None:
+#         if len(new_password) > self.PASSWORD_LENGTH:
+#             self._password = sha256(new_password.encode("utf-8")).hexdigest()
+#         else:
+#             raise ValueError("Password should be at least 8 characters long!")
+
+#     @password.deleter
+#     def delete_password(self) -> None:
+#         del self._password
+
+
+# mano_password = Passwords("1namas")
+# print(mano_password.password)
+# mano_password.set_password = "456slaptazodis"
+# print(mano_password.password)
+
+
+
+import hashlib, random
+from dataclasses import dataclass
+
+
+@dataclass
+class NewPassword:
+    old_password: str
+    new_password: str
+
+
+class WrongPasswordError(Exception):
+    pass
+
+
+class Password:
+    MIN_PASSWORD_LENGTH = 6
+    WEAK_PASSWORDS = [
+        "123456",
+        "12345678",
+        "123456789",
+        "12345",
+        "1234567",
+        "password",
+        "abcdef",
+        "abc123",
+        "qwerty",
+        "111111",
+        "1234",
+        "iloveyou",
+    ]
+
+    def __init__(self, password: str) -> None:
+        self._password = self._validate_password(password)
+
+    def _validate_password(self, password) -> str:
+        if (
+            len(password) >= self.MIN_PASSWORD_LENGTH
+            and password not in self.WEAK_PASSWORDS
+        ):
+            print("Password meets the requirements.")
+            hashed_password = self._hash_password(password=password)
+            return hashed_password
+        else:
+            return "Weak password"
+
+    @staticmethod
+    def _hash_password(password) -> str:
+        password_bytes = password.encode("utf-8")
+        hash_object = hashlib.sha256(password_bytes)
+        return hash_object.hexdigest()
+
+    def get_scrambled_hash(self) -> str:
+        hash_list = []
+        while len(hash_list) < random.randint(8, 12):
+            x = self._password[random.randrange(64)]
+            if x not in hash_list:
+                hash_list.append(x)
+        scrambled_hash = "".join(hash_list)
+        return scrambled_hash
+
+    @property
+    def password(self) -> str:
+        return self.get_scrambled_hash()
+
+    @password.setter
+    def password(self, new_password: "NewPassword") -> None:
+        compare_hash = self._hash_password(password=new_password.old_password)
+        if compare_hash == self._password:
+            self._password = self._validate_password(new_password.new_password)
+            print("New password has been set")
+        else:
+            raise WrongPasswordError("Wrong password")
+
+
+passw = Password("passwordas")
+
+print(passw.password)
+
+new_password = NewPassword(old_password="passwordas", new_password="blablablabal")
+
+try:
+    passw.password = new_password
+except WrongPasswordError as err:
+    print(err)
+
+print(passw.password)
+
+
+
+# class Person:
+#     def __init__(self, name: str) -> None:
+#         self._name = name
+
+#     @property
+#     def name(self) -> None:
+#         return self._name
+
+#     @name.setter
+#     def set_name(self, new_name: str) -> None:
+#         self._name = new_name
+
+#     @name.deleter
+#     def del_name(self):
+#         del self._name
+
+
+# class PersonOne(Person):
+#     def __init__(self, name: str) -> None:
+#         super().__init__(name)
+
+#     @property
+#     def name(self):
+#         return super().name
+
+
+# # person_one = Person(name="Vytautas")
+# # print(person_one.name)
+# # person_one.set_name = "Algirdas"
+# # print(person_one.name)
+# # person_one.del_name()
+# # print(person_one.name)
+
+# person_two = PersonOne(name="Vilius")
+# print(person_two.name)
+# person_two.name = "Mykolas"
+
+
+# Create a class UserProfile that manages user information and access privileges. 
+# Use properties with getters, setters, and deleters to control access to sensitive data and modify privileges.
+
+# Requirements:
+#  - Define properties for:
+#  - username (getter and setter)
+#  - email (getter and setter)
+#  - is_admin (getter, setter, and deleter) – This property should validate that the user is authorized to
+#    modify the is_admin attribute and provide appropriate error messages for unauthorized attempts.
+
+class UserProfile:
+    
+    def __init__(self, username: str, email: str, ) -> None:
+        pass
